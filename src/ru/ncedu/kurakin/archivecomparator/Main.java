48f138eeeea43dcd582b0c81fee2b52bdf1b08c2
MihailKurakin <wallsoftheb77@gmail.com>
2020-10-02 14:09:04 +0300

    zips_added

--- src/ru/ncedu/kurakin/archivecomparator/Main.java
+++ src/ru/ncedu/kurakin/archivecomparator/Main.java
@@ -1,14 +1,13 @@
 package ru.ncedu.kurakin.archivecomparator;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         IArchiveComparator ar = new ArchiveComparator();
-        String fp1 = ".\\zip1.zip";
-        String fp2 = ".\\zip2.zip";
+        String fp1 = ".\\zips\\zip1.zip";
+        String fp2 = ".\\zips\\zip2.zip";
         ar.loadFiles(fp1, fp2);
 
     }
